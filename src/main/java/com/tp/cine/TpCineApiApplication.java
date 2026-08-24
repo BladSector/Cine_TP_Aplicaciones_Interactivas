6a0dd8a6f2a7380d5402b0c2612d2710dbb2d34c
BladSector <lopezgg98@gmail.com>
2026-08-23 21:44:51 -0300

    Configura escaneo JPA de entidades y repositorios

--- src/main/java/com/tp/cine/TpCineApiApplication.java
+++ src/main/java/com/tp/cine/TpCineApiApplication.java
@@ -2,6 +2,8 @@ package com.tp.cine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
         "com.tp.cine",
@@ -10,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
         "service",
         "controller"
 })
+@EntityScan(basePackages = "modelo")
+@EnableJpaRepositories(basePackages = "repository")
 public class TpCineApiApplication {
 
     public static void main(String[] args) {
